4c26419f8ba2a2180695a623ae2382f329ed2a3e
Porter <126645133+LV6LASERLOTUS@users.noreply.github.com>
2025-06-18 01:28:37 -0700

    從commit 裡面移除掉暫存檔案 然後也移除掉之前練習用的爬蟲quotes.py 最後在udn_spider 裡面加上了 - json 資料裡面提取新聞的網址的方式 - 訪問每個 article_url 來獲取繁體中文資料
    下一步：
    
    - 加上udn資料處理的pipeline
    - 加上udn_spider 的proxy 與error handling
    - dcard 爬蟲

--- data_gathering/data_gathering/spiders/udn_spider.py
+++ data_gathering/data_gathering/spiders/udn_spider.py
@@ -1,16 +1,35 @@
 import scrapy
-
+from pathlib import Path
+import json
 
 class UdnSpiderSpider(scrapy.Spider):
     name = "udn_spider"
-    allowed_domains = ["udn.com"]
-    #https://udn.com/news/breaknews/1/0#breaknews
-    start_urls = ["https://udn.com/news/breaknews/1/0#breaknews"]
+    
+    #Ajax api: https://udn.com/api/more?page=0&id=&channelId=1&cate_id=0&type=breaknews
+
+    start_urls = [
+        "https://udn.com/api/more?page=0&id=&channelId=1&cate_id=0&type=breaknews",
+        "https://udn.com/api/more?page=1&id=&channelId=1&cate_id=0&type=breaknews",
+        "https://udn.com/api/more?page=2&id=&channelId=1&cate_id=0&type=breaknews"
+    ]
 
     def parse(self, response):
-        for link in response.xpath('//*[@class="story-list__text"]//a/@href'):
-            yield response.follow(link,self.parse_article)
+        BASE_URL = "https://udn.com"
+        raw_bytes = response.body
+        Path('./raw_data/response.json').write_bytes(raw_bytes)
+        json_data= json.loads(raw_bytes)
 
+        article_urls=[]
+        for article in json_data["lists"]:
+            full_url = BASE_URL + article["titleLink"]
+            article_urls.append(full_url)
+        
+        print("============ Retrieving Article ===============")
+        
+        for url in article_urls:
+            yield scrapy.Request(url, callback=self.parse_article)
+            break
+    
     def parse_article(self, response):
         yield {
             "title": response.xpath('//*[@class="article-content__title"]/text()').get(),
